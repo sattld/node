0692971e0643b983c8ab9a35850481077689588b
li chen <18410132788@163.com>
2021-07-15 01:03:30 +0800

    全部提交

--- http.js
+++ http.js
@@ -6,14 +6,49 @@ var server = http.createServer();
 server.on('request',function(request,response){
     console.log('收到请求了');
     console.log('请求路径'+request.url);
+    var url = request.url;
+    // if(url==='/'){
+    //     response.end('index page');
+    // }else if(url==='/login'){
+    //     response.end('login page');
+    // }else{
+    //     response.end('404 no found');
 
+    // }
 
+    if(url==='/products'){
+        var products = [
+            {
+                name:'苹果',
+                price:8888
+            },{
+                name:'苹果',
+                price:8888
+            },{
+                name:'苹果',
+                price:8888
+            },{
+                name:'苹果',
+                price:8888
+            },{
+                name:'苹果',
+                price:8888
+            },{
+                name:'苹果',
+                price:8888
+            },{
+                name:'苹果',
+                price:8888
+            },
+        ];
+        response.end(JSON.stringify(products));
+    }
     //响应
     // response.write('hollo');
     // response.write('node.js');
     // response.end();
 
-    response.end('hollo world');
+    // response.end('hollo world');
 })
 //绑定端口号
 server.listen(3000,function(){
